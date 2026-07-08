704e387a5897a134555af78a222d9a347a44d67e
mahmudsayed247-code <mahmudsayed247@gmail.com>
2026-07-08 22:57:23 +0300

    Add YouTube Channel Name Generator script

---
+++ main.py
@@ -0,0 +1,4 @@
+print ("Welcome to the YouTube Channel Name Generator")
+name = input("What is your nickname?\n")
+channel = input ("What is your channel about?\n")
+print (f"You could name your channel {channel} with {name}")
